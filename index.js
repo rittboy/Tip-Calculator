c581978bfb61c416a302b4cbe58a0639093751f8
John Ritter <john.ritter.8788@gmail.com>
2023-11-27 18:24:27 -0500

    Tried adding a split option, will need further testing before implementing

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const calcBtn = document.getElementById("calculate");
 const billAmt = document.getElementById("bill");
 const tipPct = document.getElementById("tip");
 const total = document.getElementById("total");
+// const people = document.getElementById("numPeople");
 /**
  * takes value of bill input, and takes percentage 
  * of tip put in, and calculates tgotal price on bill.
@@ -9,6 +10,11 @@ const total = document.getElementById("total");
 function calculateTotal(){
     const billValue = billAmt.value;
     const tipValue = tipPct.value;
+    // const splitVal = people.value;
+    // if(splitVal === "" || splitVal <= 1){
+    //     splitVal = 1;
+        
+    // }
     const totalVal = billValue *(1 + tipValue / 100);
     total.innerText = totalVal.toFixed(2);
 }
